023b83c7954b67d95844f9084a2968387833b6e7
Adarsh-Rajesh-gitHub <136111450+Adarsh-Rajesh-gitHub@users.noreply.github.com>
2026-04-03 10:55:36 -0500

    fpu mul

--- fpu.sv
+++ fpu.sv
@@ -6,15 +6,23 @@ module fpu(
 );
 
 //will comeback
+
+reg sign;
+reg [10:0] exp_1, exp_2;
+reg [52:0] mant_1, mant_2;
+reg [11:0] exp_res;
+reg [52:0] mant_res;
 always @(*) begin
     case (fpu_op)
-        5'h00: //addf
+        5'h00: begin//addf
             //align exponents by incrementing smaller exp and shifting mantissa to right till both equal, add mantissas sign same then add else subtract smaller mag form bigger mag and take sign of biggest mag, in end also check if mantiss over 1 on significant bit and if so just add 1 to exp and if mantissa such that leading bit shift left one and subtract 1 to exp
-            
             result = 64'd0; 
-        5'h01: //subf
+            
+        end
+        5'h01: begin//subf
             result = 64'd0;
-        5'h02: //mulf
+        end
+        5'h02: begin//mulf
         //exponents-bias and add both and then add the bias, multiply mantissa assuming that on eahead and rewriter
             sign = a[63] ^ b[63];
             exp_1 = a[62:52] - 1023;
@@ -24,8 +32,10 @@ always @(*) begin
             exp_res = exp_1 + exp_2 + 1023;
             mant_res = (mant_1 * mant_2) >> 52;
             result = {sign, exp_res[10:0], mant_res[51:0]};
-        5'h03: //divf
+        end
+        5'h03: begin//divf
             result = 64'd0;
+        end
         default: result = 64'd0;
     endcase
 end
